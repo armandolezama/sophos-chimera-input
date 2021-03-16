5d082c8ee26b659f16a7efbc279e10d2b7871a11
armando <armando.lezama.psic@gmail.com>
2021-03-16 05:14:29 -0600

    chore(*):added styles for individual component, properties for input configs and elements to improve demo

--- sophos-chimera-input-styles.js
+++ sophos-chimera-input-styles.js
@@ -1,3 +1,74 @@
 import { css } from 'lit-element';
 
-export default css``;
+export default css`
+  #main-container{
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    height: 200px;
+  }
+  #input-label, #input-tag, #input-tag:required {
+  -moz-transition: all 0.25s cubic-bezier(0.53, 0.01, 0.35, 1.5);
+  -o-transition: all 0.25s cubic-bezier(0.53, 0.01, 0.35, 1.5);
+  -webkit-transition: all 0.25s cubic-bezier(0.53, 0.01, 0.35, 1.5);
+  transition: all 0.25s cubic-bezier(0.53, 0.01, 0.35, 1.5);
+  }
+
+  #input-container {
+    position: relative;
+    padding: 10px 0;
+  }
+  
+  #input-label {
+    transform-origin: left center;
+    color: #ff4a56;
+    font-weight: 100;
+    letter-spacing: 0.01em;
+    font-size: 17px;
+    box-sizing: border-box;
+    padding: 10px 15px;
+    display: block;
+    position: absolute;
+    margin-top: -40px;
+    z-index: 2;
+    pointer-events: none;
+  }
+  #input-tag, #input-tag:required {
+    appearance: none;
+    background-color: none;
+    border: 1px solid #ff4a56;
+    line-height: 0;
+    font-size: 17px;
+    width: 100%;
+    display: block;
+    box-sizing: border-box;
+    padding: 10px 15px;
+    border-radius: 60px;
+    color: #ff4a56;
+    font-weight: 100;
+    letter-spacing: 0.01em;
+    position: relative;
+    z-index: 1;
+  }
+  #input-tag:focus {
+    outline: none;
+    background: #ff4a56;
+    color: white;
+    margin-top: 30px;
+  }
+  #input-tag[status="valid"] {
+    margin-top: 30px;
+  }
+  #input-tag:focus ~ label {
+    -moz-transform: translate(0, -35px);
+    -ms-transform: translate(0, -35px);
+    -webkit-transform: translate(0, -35px);
+    transform: translate(0, -35px);
+  }
+  #input-tag[status="valid"] ~ label {
+    -moz-transform: translate(5px, -35px);
+    -ms-transform: translate(5px, -35px);
+    -webkit-transform: translate(5px, -35px);
+    transform: translate(5px, -35px);
+  }
+`;
--- sophos-chimera-input.js
+++ sophos-chimera-input.js
@@ -1,5 +1,5 @@
 import { LitElement, html } from 'lit-element';
-import './sophos-chimera-input-styles';
+import styles from './sophos-chimera-input-styles';
 
 export class SophosChimeraInput extends LitElement {
   /**
@@ -9,6 +9,12 @@ export class SophosChimeraInput extends LitElement {
     */
   constructor() {
     super();
+    this.styleOfInput = '';
+    this.value = '';
+    this.isRequired = true;
+    this.label = 'Put your name here';
+    this.pattern = '';
+    this._inputStatus = '';
   };
 
   /**
@@ -16,15 +22,49 @@ export class SophosChimeraInput extends LitElement {
     */
   static get properties() {
     return {
+      styleOfInput : { type : String},
+      value : { type : String},
+      isRequired : { type : Boolean},
+      styleOfInput : { type : String},
+      label : { type : String},
+      pattern : {type : String},
+      _inputStatus : { type : String}
     };
   };
 
   static get styles() {
-    return css``;
+    return styles;
   };
 
+  _getInputValue(e) {
+    this.value = e.target.value;
+    this.validateValue();
+  }
+
+  validateValue() {
+    const validator = this.pattern !== '' ? new RegExp(this.pattern) : new RegExp('([^\s])');
+    if(this.value !== '' && validator.test(this.value)){
+      this._inputStatus = 'valid';
+    } else {
+      this._inputStatus = '';
+    };
+  };
+
+  /*Input status property sets style rule for valid or invalid */
   render() {
-    return html``;
+    return html`
+    <div id="main-container">
+      <div id="input-container">
+        <input 
+        id="input-tag"
+        status=${this._inputStatus}
+        ?required="${this.isRequired}"
+        @input="${this._getInputValue}"
+        value="${this.value}">
+        <label id="input-label">${this.label}</label>
+      </div>
+    </div>
+    `;
   };
 };
 customElements.define('sophos-chimera-input', SophosChimeraInput);
